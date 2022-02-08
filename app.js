54106c3e640618abb3d2b5b67c6e6f5c6cac3663
maxinth <tunde.j.adeleye@gmail.com>
2022-02-08 15:04:40 +0100

    printNum

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+Write a program that prints out the numbers 1 to 100 (inclusive). If the number is divisible by 3, 
+print Crackle instead of the number. If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop. You can use any language.
+ */
+
+const printNum = () => {
+  //   let num = Math.floor(Math.random() * 101);
+  //   console.log(`num is ${num}`);
+
+  for (let i = 1; i <= 100; i++) {
+    // console.log(`i is ${i}`);
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log(`CracklePop`);
+    } else if (i % 3 === 0) {
+      console.log(`Crackle`);
+    } else if (i % 5 === 0) {
+      console.log(`Pop`);
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+printNum();
