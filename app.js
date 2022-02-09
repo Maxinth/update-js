c582b6109334fcffab24fcf86f59049ce831690c
maxinth <tunde.j.adeleye@gmail.com>
2022-02-09 17:57:58 +0100

    line breaks

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let café = 1; // This constant is named "caf\u{e9}"
-café = 2; // This constant is different: "cafe\u{301}"
-café; // => 1: this constant has one value
-café; // => 2: this indistinguishable constant has a different value
+let a;
 
-console.log(café);
+a = 2;
+
+console.log(a);
