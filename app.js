79855a3894f0ed39b545b4fafd2ee19588053072
maxinth <tunde.j.adeleye@gmail.com>
2022-02-09 17:49:12 +0100

    naming too

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 // using let as a variable name declared with var
 
-var let = 2;
+const let = 2; // error, let can only be used as a variable name if the var keyword is used in declaring it
 console.log(let);
