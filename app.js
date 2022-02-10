4e05eaad9dbc4430eb103cde59d6ef53e474ff7c
maxinth <tunde.j.adeleye@gmail.com>
2022-02-10 19:15:11 +0100

    arithmetics

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const val = 0x89_ab_cd_ef; // _underscore as a BYTES separator
+let val = Math.pow(2, 53);
+val = Math.round(0.6);
+val = Math.ceil(0.6);
+val = Math.floor(0.6);
+val = Math.abs(-5);
 
 // log goes here
 console.log(val);
