201d3ae6b2e9ff359e8102b30445f522d8dde4b5
maxinth <tunde.j.adeleye@gmail.com>
2022-02-10 19:20:22 +0100

    current

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ val = Math.floor(0.6);
 val = Math.abs(-5);
 val = Math.max(9, 4, 3);
 val = Math.min(8, 4, 2);
+val = Math.random();
+val = Math.PI;
+val = Math.E;
+val = Math.sqrt(64);
+val = Math.pow(3, 1 / 3);
+val = Math.sin(0);
+val = Math.log(10);
+val = Math.LN10;
 
 // log goes here
 console.log(val);
