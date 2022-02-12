9f3a087bc2f1b91f45368de99a3fcc08f5341181
maxinth <tunde.j.adeleye@gmail.com>
2022-02-12 15:53:36 +0100

    BigInts

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // TYPES, VALUES and Variables
-let val = Infinity;
-let x = 0.3 - 0.2; // NOT .1 as you might expect
-let y = 0.2 - 0.1; // now this is 0.1
-
-console.log({ x, y });
-val = x === y;
-
+let val = 1234n;
 /**
  *
  * LOGS
