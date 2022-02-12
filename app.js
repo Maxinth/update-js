3ca886106286d47f0690ed63a3e7979cc79fad4a
maxinth <tunde.j.adeleye@gmail.com>
2022-02-12 15:35:56 +0100

    negative infinity

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 let val = Infinity;
 val = Number.POSITIVE_INFINITY; // still Infinity
 val = 1 / 0;
+
+// negative infinity
 val = -Infinity;
+val = Number.NEGATIVE_INFINITY;
+val = -1 / 0;
+val = -Number.MAX_VALUE * 2;
 
 /**
  *
