91085dd596299e717b7dee45c7310f847c76586b
maxinth <tunde.j.adeleye@gmail.com>
2022-02-12 15:39:29 +0100

    NaN

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ val = Number.NEGATIVE_INFINITY;
 val = -1 / 0;
 val = -Number.MAX_VALUE * 2;
 
+// NaN
+val = NaN;
+val = Number.NaN;
+val = 0 / 0;
+val = Infinity / Infinity;
+
+// underflow
 /**
  *
  * LOGS
  */
 // log goes here
+console.log(typeof val);
 console.log(val);
