818f63e773f62c44f2d3b3d826508937361d15fe
maxinth <tunde.j.adeleye@gmail.com>
2022-02-15 15:33:30 +0100

    more

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let val;
 val = "";
 val = "testing";
 val = 'name="myform';
+val = "Wouldn't you prefer O'Reilly's book?";
+val = "τ is the ratio of a circle's circumference to its radius";
+val = `"She said 'hi'", he said.`;
 
 /*
  * LOGS
