1c447676d92baf796b58b76f5eafa7d6151979f8
maxinth <tunde.j.adeleye@gmail.com>
2022-02-15 15:06:57 +0100

    dating ?

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 // TYPES, VALUES and Variables
-let val = 0o7777n;
+let val;
 
-val = 1000n + 2000n;
-val = 3000n - 2000n;
-val = 1n;
-val = 2000n * 3000n;
-val = 3000n / 997n;
-val = 3000n % 997n;
-val = 2n ** 131071n - 1n;
-/**
- *
+val = Date.now();
+val = new Date();
+
+val = Date().toLowerCase();
+
+/*
  * LOGS
  */
 // log goes here
