a442ee32f6129db265ba38442409964b42077258
maxinth <tunde.j.adeleye@gmail.com>
2022-02-15 15:02:09 +0100

    more on bigInt

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ let val = 0o7777n;
 val = 1000n + 2000n;
 val = 3000n - 2000n;
 val = 1n;
-
+val = 2000n * 3000n;
+val = 3000n / 997n;
+val = 3000n % 997n;
+val = 2n ** 131071n - 1n;
 /**
  *
  * LOGS
