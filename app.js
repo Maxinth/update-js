0eeb20b88ecc8894cf17c3faf85b328d8260efd2
maxinth <tunde.j.adeleye@gmail.com>
2022-02-16 15:04:08 +0100

    more strings

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 // TYPES, VALUES and Variables
 let val;
 
-val = "";
-val = "testing";
-val = 'name="myform';
-val = "Wouldn't you prefer O'Reilly's book?";
-val = "τ is the ratio of a circle's circumference to its radius";
-val = `"She said 'hi'", he said.`;
+val = `time\nlines`;
 
 /*
  * LOGS
