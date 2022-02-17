7a5789088bd4ae829c50599a5453a80e448e445a
maxinth <tunde.j.adeleye@gmail.com>
2022-02-17 13:19:24 +0100

    added axios for data fetching

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-console.log('I');
+const axios = require("axios");
 
+const getData = () => {
+  //   fetch("https://jsonplaceholder.typicode.com/todos/1")
+  //     .then((response) => response.json())
+  //     .then((json) => console.log(json));
 
-// this will be shown after 2 seconds.
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos/1555555")
+    .then((res) => console.log(res.data))
+    .catch((err) => console.log(err));
+};
 
-setTimeout(() => {
-    console.log('eat')
-}, 2000);
-
-console.log('Ice cream!')
+getData();
