09637cfc770e86ad75602e6839a9431ec10d4628
maxinth <tunde.j.adeleye@gmail.com>
2022-02-18 13:44:41 +0100

    post - second go!

--- app.js
+++ app.js
@@ -8,29 +8,16 @@ const formData = {
   password: "P@ssw0rd1!",
 };
 const getData = async () => {
-  // fetch("https://kreatesell.io/api/admin/Authorization/AddPermisions")
-  //   .then((response) => response.json())
-  //   .then((json) => console.log(json));
-
-  const response = await fetch(
-    "https://kreatesell.io/api/admin/Authorization/AddPermisions"
-  );
+  const response = await fetch(baseUrl, {
+    method: "POST",
+    body: JSON.stringify(formData),
+    header: {
+      "content-type": "application/json",
+    },
+  });
 
   const data = await response.json();
   console.log(data);
-  //   axios
-  //     .post(baseUrl, {
-  //       method: "POST",
-  //       ...formData,
-  //     })
-  //     .then((res) => {
-  //       console.log("successfully fetched");
-  //       console.log(res.data);
-  //       localStorage.setItem("token", res.token);
-  //     })
-  //     .catch((err) => {
-  //       console.error("AN ERROR OCCURRED!", err.error);
-  //     });
 };
 
 getData();
