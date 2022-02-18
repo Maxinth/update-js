c122a88de07457ae528015e31488316d27064b53
maxinth <tunde.j.adeleye@gmail.com>
2022-02-18 13:34:33 +0100

    sync and await

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ const formData = {
   username: "super.admin@kreatesell.com",
   password: "P@ssw0rd1!",
 };
-const getData = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+const getData = async () => {
+  // fetch("https://kreatesell.io/api/admin/Authorization/AddPermisions")
+  //   .then((response) => response.json())
+  //   .then((json) => console.log(json));
 
+  const response = await fetch(
+    "https://kreatesell.io/api/admin/Authorization/AddPermisions"
+  );
+
+  const data = await response.json();
+  console.log(data);
   //   axios
   //     .post(baseUrl, {
   //       method: "POST",
