da189cab9bc63c81e74b11946bc2f567fb7f213b
maxinth <tunde.j.adeleye@gmail.com>
2022-02-25 18:32:22 +0100

    spliting

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-let itemName = "Hello world";
+let itemName = "Hello, world";
+let s = itemName;
 let val = itemName.substring(1, 4);
 val = itemName.slice(1, 4);
 val = itemName.slice(-3);
+
+val = s.split(", ");
+val = s.indexOf("l");
 
 console.log(val);
