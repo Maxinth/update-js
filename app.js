039793bc41f2fd26bac0aad38bfe472238374266
maxinth <tunde.j.adeleye@gmail.com>
2022-02-25 18:29:42 +0100

    more slices

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let itemName = 'Hello world';
-let val = itemName.substring(1,4);
-val  = itemName.slice(1,4)
+let itemName = "Hello world";
+let val = itemName.substring(1, 4);
+val = itemName.slice(1, 4);
+val = itemName.slice(-3);
 
-
-console.log(val)
+console.log(val);
