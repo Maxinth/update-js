0e33387de9ddf16ed98f8c14ee84e62671502391
maxinth <tunde.j.adeleye@gmail.com>
2022-03-07 18:18:18 +0100

    pulling all data

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ const OW = runFilter("OW");
 const ST = runFilter("ST");
 const SA = runFilter("SA");
 
-// console.log({ OW, ST, SA });
+// console.log({ OW, ST, SA }); 17,20,27 === 64
+
+const OWandST = [...OW, ...ST].length;
+const OWandSA = [...OW, ...ST].length;
+const STandSA = [...ST, ...SA].length;
+
+const all = [...OW, ...SA, ...ST].length;
+
+console.log(all);
