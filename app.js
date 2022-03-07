f264502102f54324f2496c72eb012ff0ca83119b
maxinth <tunde.j.adeleye@gmail.com>
2022-03-07 18:38:05 +0100

    updated test cases

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const runFilter = (id) => {
   return initData.filter((item) => item.alliance === id);
 };
 
+// individual checks
 const OW = runFilter("OW");
 const ST = runFilter("ST");
 const SA = runFilter("SA");
 
 // console.log({ OW, ST, SA }); 17,20,27 === 64
 
+// two filters checked
 const OWandST = [...OW, ...ST].length;
 const OWandSA = [...OW, ...ST].length;
 const STandSA = [...ST, ...SA].length;
 
+// all checked
 const all = [...OW, ...SA, ...ST];
+
+// custom reusable functions - assuming all filters have been initially checked
 
 // ALL INITIALLY CHECKED, remove one.
 const filterOneFromAll = (id) => all.filter((item) => item.alliance !== id);
@@ -25,3 +30,5 @@ const filterTwoFromAll = (id, id2) =>
 
 // when all filters are unchecked
 const allFiltersUnchecked = initialData;
+
+//
